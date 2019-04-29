e43c4da699fc855a156edf0cab59cc41a6a7ea84
Skillerde6de <mennobordes@hetnet.nl>
2019-04-29 23:35:00 +0200

    Welkomst groet

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,13 @@ import { FaDesktop } from "react-icons/fa"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hello visitor</h1>
-    <p>Welcome to my new Portfolio site.</p>
+    <h1>Welkom</h1>
+    <h6>Made by Menno</h6>
+    <p>Deze website is momenteel onder constructie. </p>
+    <p>
+      De website zal worden gebruikt om een portfolio op weer te geven van mijn
+      gedane activiteiten.
+    </p>
     <FaDesktop />
   </Layout>
 )
