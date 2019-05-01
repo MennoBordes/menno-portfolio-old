eca2903f7100846bb44b32ae7f29d00dfad3d9e4
Skillerde6de <mennobordes@hetnet.nl>
2019-05-01 23:11:08 +0200

    Implemented default background image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { FaDesktop } from "react-icons/fa"
+import BackgroundSection from "../components/Globals/BackgroundSection"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Welkom</h1>
@@ -15,7 +17,25 @@ const IndexPage = () => (
       mijn gedane activiteiten.
     </p>
     <FaDesktop />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="Portfolio"
+      styleClass="default-background"
+    />
   </Layout>
 )
+
+// Below query is being used to get the "ballenbuis.png" and pass it on to the "data" in the IndexPage
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "ballenbuis.png" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
