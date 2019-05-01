fbe57b6e87ba71ecd880f57bfe59eee6b27c4e67
Skillerde6de <mennobordes@hetnet.nl>
2019-05-01 23:15:45 +0200

    404 page and timestamp
    https://youtu.be/oAVhEPey_qA?t=6120
    1:42

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>Pagina niet gevonden</h1>
+    <p>Helaas, deze pagina bestaat niet.</p>
   </Layout>
 )
 
