484f4083413ccc8c1fab88947efde34583b2779a
Skillerde6de <mennobordes@hetnet.nl>
2019-05-01 21:58:07 +0200

    Create over.js

---
+++ src/pages/over.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+import Layout from "../components/layout"
+
+export default class over extends Component {
+  render() {
+    return (
+      <Layout>
+        <h1>Welkom op de about pagina.</h1>
+      </Layout>
+    )
+  }
+}
