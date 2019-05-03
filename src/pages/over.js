ab5f0e44635e2bc73e50bc4b41fdbbc6e0d9d7cc
Skillerde6de <mennobordes@hetnet.nl>
2019-05-03 15:40:15 +0200

    Update over.js

--- src/pages/over.js
+++ src/pages/over.js
@@ -8,12 +8,35 @@ export default class over extends Component {
   render() {
     return (
       <Layout>
-        <h1 className="text-center">Welkom op de about pagina.</h1>
-        <Title title="Over mij" styleClass="text-capitalize" />
+        <Title
+          title="Over mij"
+          styleClass="text-uppercase font-weight-bold"
+          // titleOrientation="text-left"
+          headerType="2"
+        />
+        {/* Make responsive to the screen size eg: place everything in columns and display x columns side by side if screen is wide enough, else resize elements  */}
         <TextSection>
-          <p>Wie ik ben, wat ik doe</p>
+          <hr />
+          <p>
+            Ik ben momenteel student aan de{" "}
+            <a
+              href="https://www.hogeschoolrotterdam.nl/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Hogeschool Rotterdam
+            </a>
+            , waaraan ik de Bachelor Software Engineering volg.
+          </p>
+          <p className="font-weight-bold text-capitalize">Naam:</p>
+
+          <p>Menno Bordes</p>
+          <p className="font-weight-bold text-capitalize">Leeftijd:</p>
+          <p>21</p>
+          <p className="font-weight-bold text-capitalize">Locatie:</p>
+          <p>Zevenhuizen ZH, Nederland</p>
+          <hr />
           <p>foto</p>
-          <p>persoonlijke details</p>
         </TextSection>
       </Layout>
     )
