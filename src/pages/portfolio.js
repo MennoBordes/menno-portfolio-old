eabbf4c6cf4eb4f509dac69510ea931583d38c8c
Skillerde6de <mennobordes@hetnet.nl>
2019-05-03 16:43:33 +0200

    Changed h1 into title component

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,12 +2,17 @@ import React, { Component } from "react"
 
 import Layout from "../components/layout"
 import TextSection from "../components/Globals/TextSection"
+import Title from "../components/Globals/Title"
 
 export default class portfolio extends Component {
   render() {
     return (
       <Layout>
-        <h1 className="text-center">Welkom op mijn portfolio.</h1>
+        <Title
+          title="Welkom op mijn portfolio"
+          styleClass="text-uppercase font-weight-bold"
+          headerType="2"
+        />
         <TextSection>
           <p>Momenteel is mijn portfolio nog te vinden op wordpress</p>
           <a
