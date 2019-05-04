9c59a0ace16ef8db877575e83813c408af408fd8
Skillerde6de <mennobordes@hetnet.nl>
2019-05-04 22:37:39 +0200

    Renamed graphql get function

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <BackgroundSection
-      img={data.img.childImageSharp.fluid}
+      img={data.bg_img.childImageSharp.fluid}
       title="Menno Bordes"
       subTitle="Portfolio"
       styleClass="default-background"
@@ -22,7 +22,7 @@ const IndexPage = ({ data }) => (
 // Below query is being used to get the "ballenbuis.png" and pass it on to the "data" in the IndexPage
 export const query = graphql`
   {
-    img: file(relativePath: { eq: "ballenbuis.png" }) {
+    bg_img: file(relativePath: { eq: "ballenbuis.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid_tracedSVG
