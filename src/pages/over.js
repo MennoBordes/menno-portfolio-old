0fa3ae998cbdbe98fc8c7104684ae545e7949eee
Skillerde6de <mennobordes@hetnet.nl>
2019-05-04 20:14:02 +0200

    Added site header

--- src/pages/over.js
+++ src/pages/over.js
@@ -1,23 +1,47 @@
-import React, { Component } from "react"
+import React from "react"
 
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import SEO from "../components/seo"
 import Title from "../components/Globals/Title"
 import AboutInfo from "../components/About/AboutInfo"
 import PersonalInfo from "../components/About/PersonalInfo"
+import BackgroundSection from "../components/Globals/BackgroundSection"
 
-export default class over extends Component {
-  render() {
-    return (
-      <Layout>
-        <Title
-          title="Over mij"
-          styleClass="text-uppercase font-weight-bold"
-          // titleOrientation="text-left"
-          headerType="2"
-        />
-        <AboutInfo />
-        <PersonalInfo />
-      </Layout>
-    )
+// export default class over extends Component({ data }) {
+const overPage = ({ data }) => (
+  // render() {
+  // return (
+  <Layout>
+    <SEO title="Over" keywords={[`gatsby`, `application`, `react`]} />
+    <Title
+      title="Over mij"
+      styleClass="text-uppercase font-weight-bold"
+      // titleOrientation="text-left"
+      headerType="2"
+    />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="over mij"
+      styleClass="about-background"
+    />
+    <AboutInfo />
+    <PersonalInfo />
+  </Layout>
+)
+//   }
+// }
+
+// Below query is being used to get the "about-background.jpeg" and pass it on to the "data" in the IndexPage
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "about-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
   }
-}
+`
+export default overPage
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react"
+import React from "react"
 
 import Layout from "../components/layout"
 import TextSection from "../components/Globals/TextSection"
 import Title from "../components/Globals/Title"
+import SEO from "../components/seo"
 
-export default class portfolio extends Component {
-  render() {
-    return (
-      <Layout>
-        <Title
-          title="Welkom op mijn portfolio"
-          styleClass="text-uppercase font-weight-bold"
-          headerType="2"
-        />
-        <TextSection>
-          <p>Momenteel is mijn portfolio nog te vinden op wordpress</p>
-          <a
-            href="https://portfoliomennobordes.wordpress.com/portfolio-2/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className="btn text-uppercase btn-yellow">
-              wordpress portfolio
-            </button>
-          </a>
-        </TextSection>
-      </Layout>
-    )
-  }
-}
+const portfolioPage = () => (
+  <Layout>
+    <SEO title="Portfolio" keywords={[`gatsby`, `application`, `react`]} />
+    <Title
+      title="Welkom op mijn portfolio"
+      styleClass="text-uppercase font-weight-bold"
+      headerType="2"
+    />
+    <TextSection>
+      <p>Momenteel is mijn portfolio nog te vinden op wordpress</p>
+      <a
+        href="https://portfoliomennobordes.wordpress.com/portfolio-2/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <button className="btn text-uppercase btn-yellow">
+          wordpress portfolio
+        </button>
+      </a>
+    </TextSection>
+  </Layout>
+)
+
+export default portfolioPage
