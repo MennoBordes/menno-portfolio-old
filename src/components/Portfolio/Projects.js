2ae8d273b85c4fd0ce7ccfa2546df8d2464cbb1c
Skillerde6de <mennobordes@hetnet.nl>
2019-05-04 22:39:28 +0200

    Added portfolio getter from contentful
    https://youtu.be/oAVhEPey_qA?t=11855
    3:17:35

---
+++ src/components/Portfolio/Projects.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+
+export default class Projects extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: props.items.edges,
+      projectItems: props.items.edges,
+    }
+  }
+  render() {
+    const Titles = "Gemaakte projecten"
+    if (this.state.items.length > 0) {
+      // Retrieved portfolio elements
+      return (
+        <section className="projects py-5">
+          <div className="container">
+            <Title title={Titles} />
+            {/* Categories */}
+            {/* Projects */}
+            <div className="row">
+              {this.state.projectItems.map(({ node }) => {
+                return (
+                  <div
+                    key={node.id}
+                    className="col-11 col-md-6 my-3 d-flex mx-auto"
+                  >
+                    {/* Project image */}
+                    <div>
+                      <Img fixed={node.images.fixed} />
+                    </div>
+                    {/* Project text */}
+                    <div>
+                      <div className="flex-grow-1 px-3">
+                        <div className="d-flex justify-content-between">
+                          <h6 className="mb-0">
+                            <small>{node.title}</small>
+                          </h6>
+                        </div>
+                        <p className="text-muted">
+                          <small>{node.description.description}</small>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      // Didn't retrieve portfolio elements
+      return (
+        <section className="projects py-5">
+          <div className="container">
+            <Title title={Titles} />
+            <div className="row">
+              <div className="col-10 col-sm-11 mx-auto text-center text-uppercase">
+                <h1>Er zijn momenteel geen projecten om weer te geven.</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,11 +1,13 @@
 import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import TextSection from "../components/Globals/TextSection"
 import Title from "../components/Globals/Title"
 import SEO from "../components/seo"
+import Menu from "../components/Portfolio/Projects"
 
-const portfolioPage = () => (
+const portfolioPage = ({ data }) => (
   <Layout>
     <SEO title="Portfolio" keywords={[`gatsby`, `application`, `react`]} />
     <Title
@@ -25,7 +27,30 @@ const portfolioPage = () => (
         </button>
       </a>
     </TextSection>
+    <Menu items={data.projects} />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    projects: allContentfulPortfolioElements {
+      edges {
+        node {
+          id
+          title
+          category
+          description {
+            description
+          }
+          images {
+            fixed(width: 150, height: 150) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
 
 export default portfolioPage
